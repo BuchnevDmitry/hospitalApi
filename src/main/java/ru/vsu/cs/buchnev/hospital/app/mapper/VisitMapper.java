3a3c044040cf291c62aa28231b84f09c07778e53
dmitrijbuchnev <buchnev2003.03@mail.ru>
2023-12-26 23:20:11 +0300

    fix bags with VisitMapper

--- src/main/java/ru/vsu/cs/buchnev/hospital/app/mapper/VisitMapper.java
+++ src/main/java/ru/vsu/cs/buchnev/hospital/app/mapper/VisitMapper.java
@@ -7,6 +7,8 @@ import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.vsu.cs.buchnev.hospital.api.model.request.ServiceRequest;
 import ru.vsu.cs.buchnev.hospital.api.model.request.VisitRequest;
+import ru.vsu.cs.buchnev.hospital.api.model.response.DoctorResponse;
+import ru.vsu.cs.buchnev.hospital.api.model.response.ServiceResponse;
 import ru.vsu.cs.buchnev.hospital.api.model.response.VisitResponse;
 import ru.vsu.cs.buchnev.hospital.app.service.DepartmentService;
 import ru.vsu.cs.buchnev.hospital.app.service.DoctorService;
@@ -25,6 +27,30 @@ public abstract class VisitMapper {
     public abstract List<VisitResponse> mapToDto(List<VisitItem> items);
     public abstract List<VisitItem> mapToItem(List<VisitResponse> items);
     public abstract VisitItem mapToItem(VisitResponse dto);
+    protected DoctorResponse doctorItemToDoctorResponse(DoctorItem doctorItem) {
+        if ( doctorItem == null ) {
+            return null;
+        }
+
+        DoctorResponse.DoctorResponseBuilder doctorResponse = DoctorResponse.builder();
+
+        doctorResponse.id( doctorItem.getId() );
+        doctorResponse.fio( doctorItem.getFio() );
+        doctorResponse.specalization( doctorItem.getSpecalization() );
+        return doctorResponse.build();
+    }
+    protected ServiceResponse serviceItemToServiceResponse(ServiceItem serviceItem) {
+        if ( serviceItem == null ) {
+            return null;
+        }
+
+        ServiceResponse.ServiceResponseBuilder serviceResponse = ServiceResponse.builder();
+        serviceResponse.id( serviceItem.getId() );
+        serviceResponse.name( serviceItem.getName() );
+        serviceResponse.description( serviceItem.getDescription() );
+        serviceResponse.price( serviceItem.getPrice() );
+        return serviceResponse.build();
+    }
     @Mappings({
             @Mapping(target = "doctor", expression = "java(doctorService.getDoctor(request.getDoctorId()))"),
             @Mapping(target = "patient", expression = "java(patientService.getPatient(request.getPatientId()))")
